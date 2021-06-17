25bf52d86c26d3f09182407da2d4b0a5f421744c
satty26 <satyiiitp@gmail.com>
2021-06-18 00:31:50 +0530

    HERO-PAGE

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
   }, 3000);
 });
 function updateTimer() {
-  future = Date.parse("june 18, 2021 00:00:00");
+  future = Date.parse("june 20, 2021 23:59:00");
   now = new Date();
   diff = future - now;
 
